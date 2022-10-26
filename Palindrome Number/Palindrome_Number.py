c4574545d7da940e03dc39a7ca01903817c4e76c
MINT <pb1123love@gmail.com>
2022-10-27 00:46:40 +0900

    log 10 사용해서 숫자로 처리한 버전 추가
    math import 추가

--- Palindrome Number/Palindrome_Number.py
+++ Palindrome Number/Palindrome_Number.py
@@ -1,3 +1,22 @@
+from math import log10
+
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return False if x < 0 else str(x) == str(x)[::-1]
+
+
+class LogarithmSolution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if 0 < x < 10:
+            return True
+
+        digit_count = 1 + int(log10(x))
+        half = int(digit_count / 2)
+        digits = list(map(lambda exp: int(x / pow(10, exp) % 10), range(digit_count)))
+        left = digits[:half]
+        digits.reverse()
+        right = digits[:half]
+        return all(map(lambda y: y[0] == y[-1], zip(left, right)))
