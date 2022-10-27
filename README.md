2df9d16e80af749d4054fc9f35786e8b984b94b4
saranghe41 <saranghe-0-@hanmail.net>
2022-10-28 00:15:00 +0900

    RomannToInteger 문제풀기 완료

--- README.md
+++ README.md
@@ -10,3 +10,5 @@
   * 김민규 - [Python](https://github.com/ODOA-Project/ODOA/pull/7) [✅]
   * 임경미 - [c++](https://github.com/ODOA-Project/ODOA/pull/5) [✅]
   * 김지은 - [Swift](https://github.com/ODOA-Project/ODOA/pull/6) [✅]
+* 2022-10-27
+  * 김지은 - [Swift]() [✅]
---
+++ Roman to Integer/Roman_to_Integer.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class Solution {
+    func romanToInt(_ s: String) -> Int {
+        let romanToIntDics: [Character:Int] = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
+        var changeS = s.replacingOccurrences(of: "IV", with: "IIII")
+            .replacingOccurrences(of: "IX", with: "VIIII")
+            .replacingOccurrences(of: "XL", with: "XXXX")
+            .replacingOccurrences(of: "XC", with: "LXXXX")
+            .replacingOccurrences(of: "CD", with: "CCCC")
+            .replacingOccurrences(of: "CM", with: "DCCCC")
+        var sum = 0
+        
+        for ch in changeS {
+            sum += romanToIntDics[ch]!
+        }
+
+        return sum
+    }
+}
+
+let solution = Solution()
+print(solution.romanToInt("MCMXCIV"))
