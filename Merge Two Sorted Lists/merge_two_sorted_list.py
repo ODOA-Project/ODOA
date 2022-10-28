cc5675c561d1c47788142fc32aa10466b2a4ca0b
MINT <pb1123love@gmail.com>
2022-10-28 23:52:24 +0900

    recursion 으로 해결

---
+++ Merge Two Sorted Lists/merge_two_sorted_list.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next_node=None):
+        self.val = val
+        self.next = next_node
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 or not list2:
+            return list1 or list2
+
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
